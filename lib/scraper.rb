8ffb9abde070c8d394b6c9d2714b4f6236864a72
Tom Banish <tombanish@gmail.com>
2020-04-06 01:11:19 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,4 +4,4 @@ require 'open-uri'
 html = open("https://flatironschool.com/")
 
 doc = Nokogiri::HTML(open("https://flatironschool.com/"))
-doc.css(".headline-26OIBN").text
+doc.css(".headline-26OIBN").text.strip
