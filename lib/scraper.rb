445b1c38439581762cc208cb0356bb6c1fb7191d
Tom Banish <tombanish@gmail.com>
2020-04-06 01:18:17 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,4 +4,4 @@ require 'open-uri'
 html = open("https://flatironschool.com/")
 doc = Nokogiri::HTML(html)
  
-doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G").text.strip
+doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
